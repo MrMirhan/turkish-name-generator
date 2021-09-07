d6044db575ebaa9e897680d4b859bb982ec9cc66
Mirhan <hamzaalmendi@gmail.com>
2021-09-07 21:49:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
-    name='turkish-name-generatorn',
+    name='turkish-name-generator',
     packages=find_packages(include=['trnamegen']),
     include_package_data=True,
     data_files=[('', ["trnamegen/data/isimler.csv", "trnamegen/data/soyisimler.txt"])],
