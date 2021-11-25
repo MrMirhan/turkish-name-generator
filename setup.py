8435f2a8864890a7b3fd2ea06c7611991f8f800a
Mirhan <hamzamirhan.almendi@std.yeditepe.edu.tr>
2021-11-25 22:26:37 +0300

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 setup(
     name='turkish-name-generator',
     packages=find_packages(include=['trnamegen']),
     include_package_data=True,
     data_files=[('', ["trnamegen/data/isimler.csv", "trnamegen/data/soyisimler.txt"])],
-    version='1.0.4',
+    version='1.0.6',
     description='This package generates random Turkish names. You can choose the gender of name and you can get also only first name or last name.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,6 +19,5 @@ setup(
     author='MrMirhan',
     author_email='me@mirhan.net',
     license='MIT',
-    install_requires=['pandas'],
-    setup_requires=['pandas'],
+    install_requires=['pandas']
 )
